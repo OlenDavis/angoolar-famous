c04e249af8dc5dcf8f515a2f19aaec76562a15d8
Olen Davis <spelledlikeitsounds@gmail.com>
2014-08-14 13:09:39 -0700

    Almond sucks (like RequireJS), so it doesn't account for real universal module definition - specifically, anonymous module definitions. Therefore, (since Lodash uses an anonymous module definition as part of its UMD approach) I have to place this after other such scripts, changing its prefixes from 000, 001, and 002, to 010, 011, and 012.

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+rm -rf js css
+
 mkdir -pv js/vendor
 mkdir -pv css/vendor
 
-cp -fv bower_components/almond/almond.js js/vendor/000_almond.tail.js
-cp -fv bower_components/famous/dist/famous.js js/vendor/001_famous.tail.js
-cp -fv bower_components/famous-angular/dist/famous-angular.js js/vendor/002_famous_angular.tail.js
+cp -fv bower_components/almond/almond.js js/vendor/010_almond.tail.js # this has to come after the lodash specification
+cp -fv bower_components/famous/dist/famous.js js/vendor/011_famous.tail.js
+cp -fv bower_components/famous-angular/dist/famous-angular.js js/vendor/012_famous_angular.tail.js
 
 cp -fv bower_components/famous-angular/dist/famous-angular.css css/vendor/famous_angular.head.css
