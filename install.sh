680d834abd5f141c59f9acb42e7dac26f46caa87
Olen Davis <spelledlikeitsounds@gmail.com>
2014-08-18 13:27:29 -0700

    chore(upgrade): updates to the first viable RequireJS-less version of Famous
    See issue comment thread here: https://github.com/Famous/famous-angular/pull/157#issuecomment-52253424

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ rm -rf js css
 mkdir -pv js/vendor
 mkdir -pv css/vendor
 
-cp -fv bower_components/almond/almond.js js/vendor/010_almond.tail.js # this has to come after the lodash specification
-cp -fv bower_components/famous/dist/famous.js js/vendor/011_famous.tail.js
-cp -fv bower_components/famous-angular/dist/famous-angular.js js/vendor/012_famous_angular.tail.js
+cp -fv bower_components/famous/famous-global.js js/vendor/000_famous_global.tail.js
+cp -fv bower_components/famous-angular/dist/famous-angular.js js/vendor/002_famous_angular.tail.js
 
 cp -fv bower_components/famous-angular/dist/famous-angular.css css/vendor/famous_angular.head.css
