2582afeca92616d9671072722d0b9232d673a4ab
Olen Davis <spelledlikeitsounds@gmail.com>
2014-08-20 10:57:24 -0700

    removal(famous-angular): removes famous-angular from this angoolar package; this is now strictly an angoolar package for the global famous package.

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 rm -rf js css
 
 mkdir -pv js/vendor
-mkdir -pv css/vendor
+# mkdir -pv css/vendor
 
 cp -fv bower_components/famous/famous-global.js js/vendor/000_famous_global.tail.js
-cp -fv bower_components/famous-angular/dist/famous-angular.js js/vendor/002_famous_angular.tail.js
+# cp -fv bower_components/famous-angular/dist/famous-angular.js js/vendor/002_famous_angular.tail.js
 
-cp -fv bower_components/famous-angular/dist/famous-angular.css css/vendor/famous_angular.head.css
+# cp -fv bower_components/famous-angular/dist/famous-angular.css css/vendor/famous_angular.head.css
